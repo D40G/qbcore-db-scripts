0df89bfd7fcd6bc612569c1d5067bc00c6de66a3
awojnarek <andy.wojnarek@theatsgroup.com>
2022-03-31 12:30:27 +0000

    Fixing some string stuff

--- top_ten_wealth.rb
+++ top_ten_wealth.rb
@@ -26,6 +26,12 @@ end
 ########
 
 array = %x[cat vehicles.txt].split("\n")
+
+array = []
+File.readlines('vehicles.txt').each do |line|
+    array << line.strip
+end
+
 array.each do |e|
     model = e.split(" ")[0]
     price = e.split(" ")[1]
@@ -67,8 +73,15 @@ end
 wealth = wealth.sort_by {|k,v| v}.reverse
 
 count = 0
+printf("%-30s %-35s\n", "Person", "Total Wealth")
 wealth.each do |k,v|
     exit if count > 20
-    puts charinfomap[k].ljust(25) + "$"+v.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+    #printf("%-30s %-35s\n", "#{charinfomap[k]}", "$#{v.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,')}")
+
+    x = v.to_s.split('.')
+    x[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+    x = x.join('.')
+    
+    printf("%-30s %-35s\n", "#{charinfomap[k]}", "$#{x}")
     count += 1
 end
