bdac9f46c9517bad8acacc6096d706d1af710433
awojnarek <andy.wojnarek@theatsgroup.com>
2022-05-27 01:57:26 +0000

    WIP

--- duplicate-serials.rb
+++ duplicate-serials.rb
@@ -134,9 +134,9 @@ stash.each do |row|
             if e.class == Array then
                 next if e[1]["name"] =~ /evidence/
                 next if e[1]["name"] =~ /casing/
+                next if e[1]["info"] == []
+                next if e[1]["info"] == ""
                 if e[1].has_key?("info") then
-                    next if e[1]["info"] == []
-                    next if e[1]["info"] == ""
                     if e[1]["info"].has_key?("serie") then
 
                         owner = stashmap[stash]
